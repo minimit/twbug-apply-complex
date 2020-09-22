529f56db7d00e0528df8d75bab0090f316fdb907
minimit <beaver82@gmail.com>
2020-09-22 09:47:28 +0200

    test utilities and css

---
+++ tailwind-plugin.js
@@ -0,0 +1,20 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = plugin.withOptions(() => {
+  return function ({ addUtilities }) {
+
+    addUtilities({
+        '.plugin': {
+            position: 'initial',
+            '> .plugin-inner': {
+                display: 'inline-block',
+                '@apply nested-apply !important': '',
+            },
+        },
+        '.nested-apply': {
+            display: 'block',
+        },
+    }, ['responsive'])
+
+  }
+})
