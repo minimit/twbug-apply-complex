9a1721b51aad60648a618291b5cc02755c98df5a
Riccardo Caroli <rcaroli@webgriffe.com>
2021-01-25 12:31:05 +0100

    upgrade latest

--- tailwind-plugin.js
+++ tailwind-plugin.js
@@ -4,16 +4,16 @@ module.exports = plugin.withOptions(() => {
   return function ({ addUtilities }) {
 
     addUtilities({
-        '.plugin': {
-            position: 'initial',
-            '> .plugin-inner': {
-                display: 'inline-block',
-                '@apply nested-apply !important': '',
-            },
+      '.plugin': {
+        position: 'initial',
+        '> .plugin-inner': {
+          display: 'inline-block',
+          '@apply nested-apply !important': '',
         },
-        '.nested-apply': {
-            display: 'block',
-        },
+      },
+      '.nested-apply': {
+        display: 'block',
+      },
     }, ['responsive'])
 
   }
